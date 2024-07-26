515b74c00bb7d55289c626fcaf63cf7765bbc392
rohit dongare <rohitdongare2611@gmail.com>
2024-07-26 11:37:50 +0530

    creating user routes and controller

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import userRoutes from './routes/user.route.js';
+
 dotenv.config();
 
 mongoose
@@ -17,3 +19,6 @@ const app = express();
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 });
+
+app.use('/api', userRoutes);
+
