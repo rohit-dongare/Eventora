bf6f086ce58f9a0446b63e439b5bfc821feb5faf
rohit dongare <rohitdongare2611@gmail.com>
2024-07-27 16:33:27 +0530

    complete sign-in page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,114 @@
-import React from 'react'
+import { Label, TextInput, Button, Alert, Spinner } from 'flowbite-react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const SignIn = () => {
-  return (
-    <div>Signin</div>
-  )
-}
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-export default SignIn
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      return setErrorMessage('Please fill in all fields');
+    }
+
+    try {
+      setLoading(true);
+      setErrorMessage(null);
+      const res = await fetch('/api/auth/signin', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+
+      const data = await res.json();
+
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+
+      setLoading(false);
+
+      if(res.ok){
+        navigate('/');
+      }
+
+    } catch (error) {
+      setErrorMessage(error.message);
+      setLoading(false);
+    } finally{
+      setLoading(false);
+    }
+  };
+
+  return (
+    <div className='min-h-screen mt-20'>
+      <div className='flex gap-5 p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center'>
+        <div className='flex-1'>
+          <Link to='/' className="font-bold dark:text-white text-4xl">
+            <span className='px-2 py-1 bg-gradient-to-r from-teal-400 via-blue-500 to-indigo-600 hover:bg-gradient-to-l rounded-lg text-white'>
+              IdeaFusion's
+            </span>
+            Blog
+          </Link>
+          <p className='text-lg mt-5'>
+            This is a sample blog. You can sign in with your email and password or with Google.
+          </p>
+        </div>
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div>
+              <Label value='Your email' />
+              <TextInput
+                type='email'
+                placeholder='name@company.com'
+                id='email'
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value='Your password' />
+              <TextInput
+                type='password'
+                placeholder='**********'
+                id='password'
+                onChange={handleChange}
+              />
+            </div>
+            <Button className={`${loading ? 'blur-[2px]' : ''} bg-gradient-to-r from-indigo-600 via-purple-600 to-pink-600 font-bold text-white rounded-lg hover:bg-gradient-to-l`} disabled={loading} type='submit'>
+              {loading ? (
+                <>
+                  <Spinner size='sm' className='w-[18px]' />
+                  <span className='pl-3'>Loading...</span>
+                </>
+              ) : 'Sign In'}
+            </Button>
+          </form>
+          <div className='flex gap-2 text-sm mt-4'>
+            <span>Don't have an account?</span>
+            <Link to='/sign-up' className='text-blue-700 hover:text-blue-800 font-semibold'>
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className='mt-5 bg-red-200 text-red-600 h-auto rounded-none py-3 px-2 font-semibold'>
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
