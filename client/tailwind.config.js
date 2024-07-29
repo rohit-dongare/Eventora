3f0f04be83480af660cb63965c93a69bd89bb215
rohit dongare <rohitdongare2611@gmail.com>
2024-07-29 23:28:11 +0530

    make changes in tailwind config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,14 +1,17 @@
+const flowbite = require("flowbite-react/tailwind");
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     './index.html',
     './src/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    // 'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+    flowbite.content()
   ],
   theme: {
     extend: {},
   },
   plugins: [
-    require('flowbite/plugin'),
+    // require('flowbite/plugin'),
+    flowbite.plugin(),
   ],
 };
