7888644c8fc36052d94c55a552892215559dde6a
rohit dongare <rohitdongare2611@gmail.com>
2024-07-29 18:46:09 +0530

    make dashboard private

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import PrivateRoute from './Components/PrivateRoute';
 
 const App = () => {
   return (
@@ -17,7 +18,9 @@ const App = () => {
         <Route path="/about" element={<About/>} />
         <Route path="/sign-in" element={<SignIn/>} />
         <Route path="/sign-up" element={<SignUp/>} />
-        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route element={<PrivateRoute/>}>
+         <Route path="/dashboard" element={<Dashboard/>} />
+        </Route>
         <Route path="/projects" element={<Projects/>} />
       </Routes>
       <Footer/>
--- client/src/Components/Header.jsx
+++ client/src/Components/Header.jsx
@@ -25,23 +25,23 @@ const Header = () => {
   useEffect(() => {
     setTimeout(() => {
       setMobileMenu(false);
-    }, 1000)
-  }, [location])
+    }, 1000);
+  }, [location]);
 
   const openMobileMenu = () => {
     setMobileMenu(true);
-  }
+  };
 
   const closeMobileMenu = () => {
     setMobileMenu(false);
-  }
+  };
 
   const isActive = (currentPath) => {
     return path === currentPath ? "text-sky-500 font-bold" : "hover:text-sky-500";
-  }
+  };
 
   return (
-    <Navbar className={`flex items-center justify-between bg-white dark:bg-gray-900 dark:text-gray-400 py-2 px-4 z-[2] relative ${mobileMenu ? '' : 'shadow-md'}`}>
+    <Navbar className={`flex items-center justify-between bg-white dark:bg-[rgba(49,65,110,0.66)] dark:text-gray-400 py-2 px-4 z-[2] relative ${mobileMenu ? '' : 'shadow-md'}`}>
       <Link to='/' className="text-sm sm:text-xl font-semibold dark:text-white">
         <span className='px-2 py-1 bg-gradient-to-r from-teal-400 via-blue-500 to-indigo-600 hover:bg-gradient-to-l rounded-lg text-white'>
           IdeaFusion's
@@ -86,17 +86,17 @@ const Header = () => {
         {mobileMenu ? (
           <VscChromeClose onClick={closeMobileMenu} className='w-[30px] h-[30px] cursor-pointer p-1 lg:hidden transition-transform duration-200 ease' />
         ) : (
-          <SlMenu onClick={openMobileMenu} className='w-[30px] h-[30px] cursor-pointer lg:hidden transition-transform duration-200 ease bg-sky-100 dark:bg-gray-700' />
+          <SlMenu onClick={openMobileMenu} className='w-[30px] h-[30px] cursor-pointer lg:hidden transition-transform duration-200 ease bg-sky-100 dark:bg-gray-600' />
         )}
       </div>
 
-      <div className={`lg:flex gap-6 ${mobileMenu ? 'absolute top-[58px] left-0 w-full flex flex-col items-start gap-2 bg-white dark:bg-gray-900 text-gray py-4 px-6 transition-all duration-300 ease-in-out border-2 dark:border-gray-700' : 'hidden'}`}>
+      <div className={`lg:flex gap-6 ${mobileMenu ? 'absolute top-[58px] left-0 w-full flex flex-col items-start gap-2 bg-white dark:bg-gray-800 text-gray py-4 px-6 transition-all duration-300 ease-in-out border-2 dark:border-gray-700' : 'hidden'}`}>
         <Link className={`${isActive('/')} hover:text-sky-500 ${mobileMenu ? 'w-full h-5 py-2' : ''}`} to="/">Home</Link>
         <Link to="/about" className={`${isActive('/about')} hover:text-sky-500 ${mobileMenu ? 'w-full h-5 py-2' : ''}`}>About</Link>
         <Link to="/projects" className={`${isActive('/projects')} hover:text-sky-500 ${mobileMenu ? 'w-full h-5 py-2' : ''}`}>Projects</Link>
       </div>
     </Navbar>
   );
-}
+};
 
 export default Header;
